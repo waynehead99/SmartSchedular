a454c57aba3df0fec2c22d46d0d0016d882e5067
Wayne Erikson <waynehead99@hotmail.com>
2025-01-03 13:50:36 -0700

    fix: Update Flask to listen on all interfaces

--- app.py
+++ app.py
@@ -526,4 +526,4 @@ def approve_suggestion():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
